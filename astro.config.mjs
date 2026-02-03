f74a3df85b6207b77f48b3cb380b7dc454cfb47d
the-chronoscope <jgpoulain@gmail.com>
2026-02-03 15:01:32 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,10 +6,7 @@ import vercel from '@astrojs/vercel/serverless';
 
 export default defineConfig({
   output: 'hybrid',
-  adapter: vercel({
-    // Optimizes the function bundling for Vercel's specific runtime
-    includeFiles: ['./src/**/*']
-  }),
+  adapter: vercel(),
   integrations: [
     react(),
     tailwind(),
