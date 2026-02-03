badea3bbd3a2e3b4ad3f7e1b38e6b585a8e95616
the-chronoscope <jgpoulain@gmail.com>
2026-02-03 14:28:34 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,11 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 import keystatic from '@keystatic/astro';
+import vercel from '@astrojs/vercel/serverless';
 
 export default defineConfig({
-  output: 'static',
+  output: 'hybrid', // Switches to "Static by default, Dynamic where needed"
+  adapter: vercel(), // Tells Vercel how to handle the dynamic Admin parts
   integrations: [
     react(),
     tailwind(),
