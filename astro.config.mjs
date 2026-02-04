93dddb9739d6fdf7e342fbbcc31b12bc32178f51
the-chronoscope <jgpoulain@gmail.com>
2026-02-04 15:38:33 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 import keystatic from '@keystatic/astro';
-import vercel from '@astrojs/vercel/serverless';
+import vercel from '@astrojs/vercel';
 
 export default defineConfig({
   output: 'server', // Changed to 'server' to enforce dynamic runtime for Auth
