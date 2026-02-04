a3169923389199b79a22b156172728a77e2bf584
the-chronoscope <jgpoulain@gmail.com>
2026-02-04 14:58:19 -0500

    Update keystatic.config.ts

--- keystatic.config.ts
+++ keystatic.config.ts
@@ -1,17 +1,17 @@
 import { config, fields, collection } from '@keystatic/core';
 
 export default config({
-  storage: import.meta.env.PROD
-    ? {
-        kind: 'github',
-        repo: {
-          owner: 'the-chronoscope',
-          name: 'chronoscope-studio',
-        },
-      }
-    : {
-        kind: 'local',
-      },
+  // HARDCODED CREDENTIALS TEST
+  storage: {
+    kind: 'github',
+    repo: {
+      owner: 'the-chronoscope',
+      name: 'chronoscope-studio',
+    },
+    clientId: 'Iv23liEhoBI5d9KirHUn',
+    clientSecret: 'e8cf760719d2cc1fb6e0f338d033e5c2a6ee65a5',
+  },
+  // ... rest of file (collections) ...
   collections: {
     articles: collection({
       label: 'Articles',
